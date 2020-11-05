32ee3a3c7dd0bca5dce491e1eb1df3b897407b59
pommetea <72635635+pommetea@users.noreply.github.com>
2020-11-05 10:13:47 -0800

    updated CSS and started to clean up code

--- js/main.js
+++ js/main.js
@@ -6,18 +6,20 @@ let bombName = 'bomb'; // class name for a bomb
 let blankName = 'safe' // class name for a blank square
 let bombText = '💣'; // what the innerText of a bomb square will say
 let blankTile = ''; // what the innerText of a blank square will say
+const numOfBombs = 10; // number of bombs in the game
+const numOfBlanks = tileNum - numOfBombs; //number of blank squares in the game. We are subtracting one because the first in the shuffled array will be safe for the first click
+
+
+
+// State variables ===============================================
 let boardDivs = [];
 let isGameOver = false;
 let firstTileIsClicked = false;
 let bombsFlagged = 0;
 let blankFlagged = 0;
 const boardTiles = []; 
-const numOfBombs = 10; // number of bombs in the game
-const numOfBlanks = tileNum - numOfBombs; //number of blank squares in the game. We are subtracting one because the first in the shuffled array will be safe for the first click
 
-// we want access to the images
-// and we want to know what beats whats
-// so maybe a "beats property somewhere"
+// cached element references ========================================
 
 
 // Creating the grid for the game ==================================
@@ -42,12 +44,7 @@ function createBoard() {
         }
       })
 
-    // tile.addEventListener('contextmenu', function(ev) { //context menu is used to detect a right click since right clicking brings up a context menu
-    //     ev.preventDefault();
-    //     flagTile(ev);
-    //         });
-
-    tile.oncontextmenu = function(e) {
+      tile.oncontextmenu = function(e) {
         e.preventDefault()
         flagTile(tile)
       }
@@ -62,46 +59,33 @@ function createBoard() {
     boardArray.push(...bombArray);
     boardArray.push(...blankArray);
 
- 
-
-    function shuffle(){ // we are randomizing the order of the bombs and blank areas
+     function shuffle(){ // we are randomizing the order of the bombs and blank areas
         const shuffled = boardArray.sort(() => Math.random() - 0.5);
         boardTiles.push(...shuffled);
         }
    
     shuffle();
-
-    
-}// last bracket of createBoard
-// createBoard();
+  }
 //====================================================================
 
-
-// State variables ===============================================
-
-
-
-
-// cached element references ========================================
 
 
 // event listeners ================================================================
 window.addEventListener('load', createBoard());
 
- //cannot be a bomb, so will be a number or empty space
-function firstClick(tile){
+function firstClick(tile){ //cannot be a bomb, so will be a number or empty space
     let clickedId = parseInt(tile.id); 
     tile.className = 'safe';
     console.log(clickedId)
 for (i = 0; i < width * width; i++){ // we are adding class names to each div with a number as an id so each square is labeled as either bomb or safe
     let gridBlocks = document.getElementById([i]);
-    if(gridBlocks.className === 'safe') { continue; }
+    if(gridBlocks.className === 'safe') { continue; } // since the first tile we clicked has already been assigned 'safe', we will skip over it and assign classes to the rest
     gridBlocks.className = boardTiles[i];
-}
-firstTileIsClicked = true;
+    }
+firstTileIsClicked = true; // once the first click is made, firstTileisClicked is triggered so that the next click will just be regular click
 
-//add numbers to squares
-for (let i = 0; i < boardTiles.length; i++){
+
+for (let i = 0; i < boardTiles.length; i++){ //add numbers to squares
 let bombTotal = 0;    
 const isLeftSide = i % width === 0; //defines which div numbers are on the left side of the grid
 const isRightSide = i % width === width - 1; //defines which div numbers are on the right side of the grid
@@ -115,17 +99,15 @@ const isRightSide = i % width === width - 1; //defines which div numbers are on 
         if(i < width * width - width && !isLeftSide && boardDivs[i + width - 1].classList.contains('bomb')) bombTotal++//checks bottom left square
         if(i < width * width - width && boardDivs[i + width].classList.contains('bomb')) bombTotal++ //checks below square
         if(i < width * width - width && !isRightSide && boardDivs[i + width + 1].classList.contains('bomb')) bombTotal++ //checks bottom right square
-        boardDivs[i].data = bombTotal;
+        boardDivs[i].data = bombTotal; // we are storing the bombTotal into the div's data to refer to later to display
         if(bombTotal == 0){
-        boardDivs[i].data = 'empty';
+        boardDivs[i].data = 'empty'; // we are storing empty so we know that this will be a blank tile later
         }
     } else {
         boardDivs[i].data = 'bomb';
-        boardDivs[i].classList.add('highlight');
     }
-
 }
-}
+} // end of firstClick() function
 
      function click(tile){
         // console.log(tile)
